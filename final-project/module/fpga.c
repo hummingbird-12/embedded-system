bb7ae27914cd7424e679f6b8bd47da0cdb927095
hummingbird-12 <entropyih@sogang.ac.kr>
2020-06-27 21:16:39 +0900

    Final - Work on FPGA interface

--- final-project/module/fpga.c
+++ final-project/module/fpga.c
@@ -2,8 +2,7 @@
 
 static char* fpga_addr[FPGA_DEVICES_CNT];
 
-static const int FPGA_HW_ADDR[] = {BUZZER_ADDRESS, DOT_ADDRESS,
-                                   FND_ADDRESS,    LED_ADDRESS,
+static const int FPGA_HW_ADDR[] = {DOT_ADDRESS, FND_ADDRESS, LED_ADDRESS,
                                    SWITCH_ADDRESS, TEXT_LCD_ADDRESS};
 
 /*
@@ -12,15 +11,14 @@ static const int FPGA_HW_ADDR[] = {BUZZER_ADDRESS, DOT_ADDRESS,
 void fpga_iomap_devices(void) {
     logger(INFO, "[fpga] IO-mapping FPGA devices\n");
 
-    fpga_addr[BUZZER] = ioremap(FPGA_HW_ADDR[BUZZER], 0x1);
     fpga_addr[DOT] = ioremap(FPGA_HW_ADDR[DOT], 0xA);
     fpga_addr[FND] = ioremap(FPGA_HW_ADDR[FND], 0x2);
     fpga_addr[LED] = ioremap(FPGA_HW_ADDR[LED], 0x1);
     fpga_addr[SWITCH] = ioremap(FPGA_HW_ADDR[SWITCH], 0x18);
     fpga_addr[TEXT_LCD] = ioremap(FPGA_HW_ADDR[TEXT_LCD], 0x32);
 
-    const char* FPGA_DEVICES_STR[] = {"BUZZER", "DOT",    "FND",
-                                      "LED",    "SWITCH", "TEXT_LCD"};
+    const char* FPGA_DEVICES_STR[] = {"DOT", "FND", "LED", "SWITCH",
+                                      "TEXT_LCD"};
     for (dev = 0; dev < FPGA_DEVICES_CNT; dev++) {
         if (fpga_addr[dev] == NULL) {
             logger(ERROR, "Failed to IO-map device %s\n",
@@ -69,4 +67,61 @@ void fpga_fnd_write(const int score) {
     }
 
     outw(value, (unsigned int) fnd_addr);
+}
+
+/*
+ *
+ */
+void fpga_led_write(const int lives) {
+    const unsigned short value = ((lives > 0) ? (0xFF << (8 - digit)) : 0);
+
+    logger(INFO, "Turning on LED %d of LED device\n", digit);
+
+    outw(value, (unsigned int) fpga_addr[LED]);
+}
+
+/*
+ *
+ */
+int fpga_switch_read() {
+    int i;
+    unsigned short int _s_value;
+
+    for (i = 0; i < length; i++) {
+        _s_value = inw((unsigned int) fpga_addr[SWITCH] + i * 2);
+        if (_s_value & 0xFF != 0) {
+            return i;
+        }
+    }
+}
+
+/*
+ *
+ */
+void fpga_text_lcd_write(const char* available) {
+    int i;
+    unsigned short int value;
+    unsigned char buffer[TEXT_LCD_BUFFER_SIZE + 1] = {0};
+
+    memset(buffer, ' ', TEXT_LCD_BUFFER_SIZE);
+
+    for (i = 0; i < 13; i++) {
+        buffer[i + 2] = (available[i] != 0 ? 'A' + i : ' ');
+    }
+
+    buffer[16] = '\0';
+    logger(INFO, "Writing line 1 \"%-16s\" into Text LCD device\n", buffer);
+    buffer[16] = ' ';
+
+    for (i = 13; i < 26; i++) {
+        buffer[i + 5] = (available[i] != 0 ? 'A' + i : ' ');
+    }
+
+    logger(INFO, "Writing line 2 \"%-16s\" into Text LCD device\n",
+           buffer + 16);
+
+    for (i = 0; i < TEXT_LCD_BUFFER_SIZE; i += 2) {
+        value = (buffer[i] & 0xFF) << 8 | (buffer[i + 1] & 0xFF);
+        outw(value, (unsigned int) fpga_addr[TEXT_LCD] + i);
+    }
 }
--- final-project/module/fpga.h
+++ final-project/module/fpga.h
@@ -1,16 +1,17 @@
 #ifndef _FPGA_H_INCLUDED_
 #define _FPGA_H_INCLUDED_
 
-#define FPGA_DEVICES_CNT 6
+#define FPGA_DEVICES_CNT 5
 
-#define BUZZER_ADDRESS 0x08000070
 #define DOT_ADDRESS 0x08000210
 #define FND_ADDRESS 0x08000004
 #define LED_ADDRESS 0x08000016
 #define SWITCH_ADDRESS 0x08000050
 #define TEXT_LCD_ADDRESS 0x08000090
 
-typedef enum _FPGA_DEVICES { BUZZER, DOT, FND, LED, SWITCH, TEXT_LCD } fpga;
+#define TEXT_LCD_BUFFER_SIZE 32
+
+typedef enum _FPGA_DEVICES { DOT, FND, LED, SWITCH, TEXT_LCD } fpga;
 
 void fpga_iomap_devices(void);
 void fpga_iounmap_devices(void);
